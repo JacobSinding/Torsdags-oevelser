03f316c8aa458d942ca3880b72a0c1b463ca27a9
JacobSinding <71074571+JacobSinding@users.noreply.github.com>
2020-11-19 14:52:06 +0100

    Uge 47

---
+++ torsdagsOpgUge47sql.sql
@@ -0,0 +1,24 @@
+use sakila;
+/*select * FROM actor where last_name = "cage"'*/
+/*select * FROM actor where first_name not IN ('Zero', 'Nick');*/
+/*select * FROM actor where first_name IN ("NICK", "JOHNNY", "JAMES", "MORGAN", "WHOOPI")*/
+/*SELECT * FROM actor WHERE actor_id between '50' and '150';*/
+/*select * FROM actor where first_name LIKE 'c%'*/
+/*select * From actor order by first_name*/
+/*select * FROM actor order by last_name DESC;*/
+/*select count(*) from actor;*/
+/*select * FROM category where name IN ("horror")*/
+/*insert into actor(first_name, last_name) VALUES ('Jacob', 'Sinding');
+insert into film_actor(actor_id, film_id) VALUES ('201','13'),('201', '14'),('201','15'),('201','16'),('201','17');*/
+/*update actor set first_name = 'Leonardo' where actor_id = 201;
+update actor set last_name = 'DeSindawg' where actor_id = 201;
+select * from actor where actor_id = 201*/
+/*delete from film_actor where actor_id = 201;
+delete from actor where actor_id = 201;*/
+
+
+
+
+
+
+
